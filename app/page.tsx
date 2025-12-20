6f33484605eecb4b85b0bfc188267eb6ebf050c8
pankaj <pankajteceract@gmail.com>
2025-12-20 18:33:14 +0530

    Add trpc-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
-import prisma from '@/lib/db'
+import { getQueryClient, trpc } from '@/trpc/server';
+import { Client } from './client';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Suspense } from 'react';
+export default function Home() {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions());
 
-export default async function Home() {
-  const users = await prisma.user.findMany();
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
-      <h1 className="text-4xl font-bold mb-8 font-(family-name:--font-geist-sans) text-[#333333]">
-        Superblog
-      </h1>
-      <ol className="list-decimal list-inside font-(family-name:--font-geist-sans)">
-        {users.map((user) => (
-          <li key={user.id} className="mb-2">
-            {user.name}
-          </li>
-        ))}
-      </ol>
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<p>Loading....</p>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
     </div>
   );
 }
