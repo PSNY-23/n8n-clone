12c04a4c9cc7bcac0dbfe7855719941efd0dee70
pankaj <pankajteceract@gmail.com>
2025-12-23 13:11:45 +0530

    Add ai-providers(gemini, openai, anthropic sdk by vercel)

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { LogoutButton } from './LogoutButton';
 import { useTRPC } from '@/trpc/client';
 import { Button } from '@/components/ui/button';
+import { toast } from 'sonner';
 
 const Page = () => {
   // await requireAuth();
@@ -19,6 +20,10 @@ const Page = () => {
       },
     })
   );
+
+  const testAI = useMutation(trpc.testAI.mutationOptions({
+    onSuccess: () => toast.success('AI job queued')
+  }));
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center justify-center -mt-16">
       <p>Proteced server component</p>
@@ -27,6 +32,13 @@ const Page = () => {
         Create workflow
       </Button>
       <LogoutButton />
+      <div className="w-32 h-32 bg-blue-300 border p-4">
+        <h1>Gemini ai test</h1>
+        <div>{JSON.stringify(testAI.data)}</div>
+        <Button disabled={testAI.isPending} onClick={() => testAI.mutate()}>
+          Test AI
+        </Button>
+      </div>
     </div>
   );
 };
--- inngest/functions.ts
+++ inngest/functions.ts
@@ -1,10 +1,44 @@
-import { inngest } from "./client";
+import { inngest } from './client';
+import { generateText } from 'ai';
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+//Import ai-sdk for diff providers
+import { createGoogleGenerativeAI } from '@ai-sdk/google';
+import { createOpenAI } from '@ai-sdk/openai';
+import { createAnthropic } from '@ai-sdk/anthropic';
+
+//Client instance for each provider
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const executeAI = inngest.createFunction(
+  { id: 'execute-ai' },
+  { event: 'execute/ai' },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "10s");
-    return { message: `Hello ${event.data.email}!` };
-  },
+    await step.sleep('wait-for', '20s');
+
+    const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text', generateText, {
+      model: google('gemini-2.5-flash'),
+      system: 'You are a helpful assistant',
+      prompt: 'What is 2+2 ?',
+    });
+
+    const { steps: openaiSteps } = await step.ai.wrap('openai-generate-text', generateText, {
+      model: openai('gpt-3.5-turbo-1106'),
+      system: 'You are a helpful assistant',
+      prompt: 'What is 2+2 ?',
+    });
+
+    const { steps: anthropicSteps } = await step.ai.wrap('anthropic-generate-text', generateText, {
+      model: anthropic('claude-haiku-4-5'),
+      system: 'You are a helpful assistant',
+      prompt: 'What is 2+2 ?',
+    });
+
+    return {
+      geminiSteps,
+      openaiSteps,
+      anthropicSteps,
+    };
+  }
 );
