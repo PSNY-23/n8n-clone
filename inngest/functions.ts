79bf4e9f4a8913255c61e75d50e26f7e1e90e719
pankaj <pankajteceract@gmail.com>
2025-12-23 16:08:39 +0530

    Add sentry-setup for error tracking

--- inngest/functions.ts
+++ inngest/functions.ts
@@ -5,6 +5,7 @@ import { generateText } from 'ai';
 import { createGoogleGenerativeAI } from '@ai-sdk/google';
 import { createOpenAI } from '@ai-sdk/openai';
 import { createAnthropic } from '@ai-sdk/anthropic';
+import * as Sentry from '@sentry/nextjs';
 
 //Client instance for each provider
 const google = createGoogleGenerativeAI();
@@ -15,24 +16,43 @@ export const executeAI = inngest.createFunction(
   { id: 'execute-ai' },
   { event: 'execute/ai' },
   async ({ event, step }) => {
-    await step.sleep('wait-for', '20s');
+    await step.sleep('wait-for', '5s');
+
+    Sentry.logger.info('User triggered test log', { log_source: 'sentry_test' });
+    console.warn('Something is missing');
+    console.error('This is an error i want to track');
 
     const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text', generateText, {
       model: google('gemini-2.5-flash'),
       system: 'You are a helpful assistant',
       prompt: 'What is 2+2 ?',
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     const { steps: openaiSteps } = await step.ai.wrap('openai-generate-text', generateText, {
       model: openai('gpt-3.5-turbo-1106'),
       system: 'You are a helpful assistant',
       prompt: 'What is 2+2 ?',
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     const { steps: anthropicSteps } = await step.ai.wrap('anthropic-generate-text', generateText, {
       model: anthropic('claude-haiku-4-5'),
       system: 'You are a helpful assistant',
       prompt: 'What is 2+2 ?',
+      experimental_telemetry: {
+        isEnabled: true,
+        recordInputs: true,
+        recordOutputs: true,
+      },
     });
 
     return {
---
+++ sentry.server.config.ts
@@ -0,0 +1,28 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://950e4d3c6574f9a1a1175cd5854916dd@o4510583561125888.ingest.us.sentry.io/4510583592517632',
+  integrations: [
+    // Add the Vercel AI SDK integration to sentry.server.config.ts
+    Sentry.vercelAIIntegration({
+      recordInputs: true,
+      recordOutputs: true,
+    }),
+    // send console.log, console.warn, and console.error calls as logs to Sentry
+    Sentry.consoleLoggingIntegration({ levels: ['log', 'warn', 'error'] }),
+  ],
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+  sendDefaultPii: true,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
